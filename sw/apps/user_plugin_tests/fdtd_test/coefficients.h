eb12e741adfa778831084e95045b84ac4e40d1e4
wenzheng73 <1830948814@qq.com>
2021-08-22 19:40:03 +0800

    update some format

--- sw/apps/user_plugin_tests/fdtd_test/coefficients.h
+++ sw/apps/user_plugin_tests/fdtd_test/coefficients.h
@@ -1,11 +1,11 @@
 //Here the update coefficient term of the field update process is placed
 //The specific coefficients are adjusted depending on the medium of the problem space
 //Here we consider only the simplest case, the electromagnetic field propagation process in free space
-#ifndef    _FDTD_COES_CONSTANT_
-#define    _FDTD_COES_CONSTANT_
-#define    COES_N    7
+#ifndef  _FDTD_COES_CONST_
+#define  _FDTD_COES_CONST_
+#define  COES_N    7
 int coes[COES_N] = {
-	   0x00200000,	
+       0x00200000,	
        0x2A5A53B9,
        0xFFF5285D,
        0x00200000,
