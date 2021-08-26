f48fea4a04465f66180b4bef04c46ffb7f9aeb09
wenzheng73 <1830948814@qq.com>
2021-08-26 17:42:47 +0800

    update some logic based-burst

--- rtl/user_plugin/rtl/fdtd/fdtd_mem_ctrl.sv
+++ rtl/user_plugin/rtl/fdtd/fdtd_mem_ctrl.sv
@@ -1,8 +1,8 @@
 module fdtd_mem_ctrl
 #(
-    parameter REG_SIZE_WIDTH = 16,
-    parameter AXI4_LENTH     = 16,
-    parameter BUFFER_SIZE    = 50
+    parameter REG_SIZE_WIDTH   = 16,
+    parameter AXI4_LENTH_WIDTH = 8,
+    parameter BUFFER_SIZE      = 50
 )
 (
     input logic     ACLK,
@@ -15,7 +15,7 @@ module fdtd_mem_ctrl
     input  logic                            ctrl_int_en_i,
     input  logic                            cmd_clr_int_pulse_i,
     input  logic                            cmd_trigger_pulse_i,
-
+    //
     output logic                            status_busy_o,
     output logic                            status_int_pending_o,
     output logic                            int_o,
@@ -68,18 +68,16 @@ module fdtd_mem_ctrl
 	      bit_depth = bit_depth >> 1;                                 
 	    end                                                           
 	  endfunction*/
-    //logic [7:0] burst_size_byte;
-    //assign burst_size_byte = AXI4_LENTH;
     /////////////////////////////////////////
     // Data Reading/Writing Processing //////
     /////////////////////////////////////////
-    logic [mstr.AXI_ADDR_WIDTH-2-1: 0]     r_r_Hy_word_addr;  // Read Hy address by word
-    logic [mstr.AXI_ADDR_WIDTH-2-1: 0]     r_r_Ez_word_addr;  // Read Ez address by word
-    logic [mstr.AXI_ADDR_WIDTH-2-1: 0]     r_r_Ez_word_addr_0;// Read Ez address by word
-    logic [mstr.AXI_ADDR_WIDTH-2-1: 0]     r_r_word_addr;     // Read    address by word
-    logic [mstr.AXI_ADDR_WIDTH-2-1: 0]     r_w_Hy_word_addr;  // Write Hy address by word
-    logic [mstr.AXI_ADDR_WIDTH-2-1: 0]     r_w_Ez_word_addr;  // Write Ez address by word
-    logic [mstr.AXI_ADDR_WIDTH-2-1: 0]     r_w_word_addr;     // Write    address by word
+    logic [mstr.AXI_ADDR_WIDTH -1: 0]      r_r_Hy_word_addr;  // Read Hy address by word
+    logic [mstr.AXI_ADDR_WIDTH -1: 0]      r_r_Ez_word_addr;  // Read Ez address by word
+    logic [mstr.AXI_ADDR_WIDTH -1: 0]      r_r_Ez_word_addr_0;// Read Ez address by word
+    logic [mstr.AXI_ADDR_WIDTH -1: 0]      r_r_word_addr;     // Read address by word
+    logic [mstr.AXI_ADDR_WIDTH -1: 0]      r_w_Hy_word_addr;  // Write Hy address by word
+    logic [mstr.AXI_ADDR_WIDTH -1: 0]      r_w_Ez_word_addr;  // Write Ez address by word
+    logic [mstr.AXI_ADDR_WIDTH -1: 0]      r_w_word_addr;     // Write address by word
     logic [REG_SIZE_WIDTH - 2 - 1: 0]      r_word_size;       // Remaining number of words
 
     logic                                  s_r_req;  // Read memory request
@@ -109,6 +107,8 @@ module fdtd_mem_ctrl
     logic                                  buffer_Ez_last_0;
     logic                                  buffer_Ez_last_1;
     logic                                  nxt_buffer_en;
+    logic [AXI4_LENTH_WIDTH-1:0]           axi4_lenth;
+    logic [AXI4_LENTH_WIDTH-1:0]           burst_size_byte;
     //record write field_data number
     logic [REG_SIZE_WIDTH-1:0]             record_num_cnt;      
     //
@@ -132,8 +132,11 @@ module fdtd_mem_ctrl
                         WAIT_2
                      } r_CS, s_CS_n; 
     //add some logic
-    //A very simple process
-    //assign s_w_data = {s_r_data[$size(s_r_data) - 2: 0], 1'b0};
+    assign axi4_lenth        = (r_CS == WAIT_WRITE_HY || r_CS == WAIT_WRITE_EZ || (calc_Hy_start_en_i && r_CS == WAIT_READ_HY ) ||
+                                (calc_Ez_start_en_i && r_CS == WAIT_READ_EZ)) ? BUFFER_SIZE : (((calc_Hy_start_en_i && r_CS == WAIT_READ_EZ ) ||
+                                (calc_Ez_start_en_i && r_CS == WAIT_READ_HY)) ? BUFFER_SIZE+1'b1 : 'd1);
+    assign burst_size_byte   = ((calc_Hy_start_en_i && r_CS == WAIT_READ_EZ) || (calc_Ez_start_en_i && r_CS == WAIT_READ_HY))
+                                         ? ((BUFFER_SIZE+1'b1) * (mstr.AXI_ADDR_WIDTH/8)) : (BUFFER_SIZE * (mstr.AXI_ADDR_WIDTH/8));
     //data_mem -> ram_buffer
     //write field_value data
     assign s_w_data          = wt_Hy_sgl ? Hy_n_i : ((wt_Ez_sgl||wt_src_sgl) ? Ez_n_i : 'd0);
@@ -146,12 +149,13 @@ module fdtd_mem_ctrl
 	                                     ? r_w_Ez_word_addr : 'd0);
 
     //signal of writing field_value data to data_mem 
-    assign wrtvalid_sgl_o    = ((r_CS == WAIT_WRITE_HY || r_CS == WAIT_WRITE_EZ || r_CS == WAIT_WRITE_SRC)
-	                           && mstr.aw_valid && mstr.aw_ready) ? 1'b1 : 1'b0;
+    assign wrtvalid_sgl_o    = ((r_CS == WAIT_WRITE_HY || r_CS == WAIT_WRITE_EZ) && (record_num_cnt <= BUFFER_SIZE-1'b1)) 
+                                         ? 1'b1 : (r_CS == WAIT_WRITE_SRC ? 1'b1 : 1'b0);
+
     //data_mem -> ram_buffer
     //read data valid signal 
-    assign rdvalid_Hy_o_o    = (rd_Hy_sgl & mstr.r_valid & mstr.r_ready);  
-    assign rdvalid_Ez_o_o    = ((rd_Ez_sgl||rd_src_sgl) & mstr.r_valid & mstr.r_ready); 
+    assign rdvalid_Hy_o_o    = (rd_Hy_sgl && mstr.r_valid && mstr.r_ready);  
+    assign rdvalid_Ez_o_o    = ((rd_Ez_sgl||rd_src_sgl) && mstr.r_valid && mstr.r_ready); 
 
     //read field_value data
     assign Hy_old_o          = rd_Hy_sgl ? s_r_data : 'd0;
@@ -187,9 +191,6 @@ module fdtd_mem_ctrl
 
     always_comb
     begin
-        s_r_req        = 1'b0;
-        s_w_req        = 1'b0;
-
         calc_Hy_flg_o  = 1'b0;
         calc_Ez_flg_o  = 1'b0;
         calc_src_flg_o = 1'b0;
@@ -219,31 +220,33 @@ module fdtd_mem_ctrl
             IDLE:
             begin
                 //status_busy_o = 1'b0;
-                s_r_req       = 1'b0;
                 if (~fdtd_start_signal_i)begin
                     s_CS_n = IDLE;
                 end
                 else
                 begin
-                    s_CS_n            = INIT_READ_HY;
-                    buffer_Hy_start_o = 1'b1;
+                    if (calc_Hy_start_en_i || calc_Ez_start_en_i)begin
+                        s_CS_n            = INIT_READ_HY;
+                        buffer_Hy_start_o = 1'b1;
+                    end
+                    else 
+                        s_CS_n = IDLE;
                 end
             end
 
             INIT_READ_HY:
             begin
                 s_CS_n       = WAIT_READ_HY;
+                rd_Hy_sgl    = 1'b1;
                 mem_rd_end_o = 1'b0;
             end
 
             WAIT_READ_HY:
-            begin
-                s_r_req           = 1'b1;
+            begin 
                 buffer_Hy_start_o = 1'b0;
                 rd_Hy_sgl         = 1'b1;
                 if (buffer_Hy_last_0||buffer_Hy_last_1)begin
                     buffer_Hy_end_o   = 1'b1;
-                    s_r_req           = 1'b0;
                     s_CS_n            = INIT_READ_EZ;
                     buffer_Ez_start_o = 1'b1;
                 end
@@ -252,18 +255,16 @@ module fdtd_mem_ctrl
             INIT_READ_EZ:
             begin
                 s_CS_n    = WAIT_READ_EZ;
-                rd_Hy_sgl = 1'b0;
-            end
+                rd_Ez_sgl = 1'b1;
+			end
 
             WAIT_READ_EZ:
             begin
                 buffer_Hy_end_o   = 1'b0;
-                s_r_req           = 1'b1;
                 buffer_Ez_start_o = 1'b0;
                 rd_Ez_sgl         = 1'b1;
                 if (buffer_Ez_last_0||buffer_Ez_last_1)begin
                     buffer_Ez_end_o = 1'b1;
-                    s_r_req       = 1'b0;
                     s_CS_n        = INIT_CALC_PROCESS;
                 end
             end
@@ -323,9 +324,9 @@ module fdtd_mem_ctrl
 
             WAIT_WRITE_HY:
             begin
-                s_w_req   = 1'b1;
                 wt_Hy_sgl = 1'b1;
-                if (r_word_size == 'd0)begin
+                //if (r_word_size == 'd0&&s_w_gnt)begin
+                /*if (r_word_size == 'd0&&s_w_gnt)begin
                     mem_rd_end_o = 1'b1;
                     s_CS_n       = WAIT_0;
                 end
@@ -338,7 +339,26 @@ module fdtd_mem_ctrl
                     s_CS_n         = INIT_READ_HY;
                 end
                 else 
-                    s_CS_n = WAIT_WRITE_HY;
+                    s_CS_n = WAIT_WRITE_HY;*/
+                if (~s_w_gnt)begin
+                    s_CS_n = WAIT_WRITE_HY; 
+                end
+                else begin
+					if (r_word_size == 'd0)begin
+                        mem_rd_end_o = 1'b1;
+                        s_CS_n       = WAIT_0;
+                    end
+                    else if (nxt_buffer_en)
+                        begin
+                            wt_Hy_sgl      = 1'b0;
+                            buffer_Hy_start_o = 1'b1;
+                            mem_rd_Hy_en_o = 1'b0;
+                            mem_rd_end_o   = 1'b1;
+                            s_CS_n         = INIT_READ_HY;
+                        end
+                    else 
+                        s_CS_n = WAIT_WRITE_HY;
+                end
             end
 
             WAIT_0:
@@ -358,9 +378,8 @@ module fdtd_mem_ctrl
 
             WAIT_WRITE_EZ:
             begin
-                s_w_req   = 1'b1;
                 wt_Ez_sgl = 1'b1;
-                if (r_word_size == 'd1)begin
+                /*if (r_word_size == 'd0&&s_w_gnt)begin
                     mem_rd_end_o = 1'b1;
                     s_CS_n       = WAIT_1;
                 end
@@ -373,7 +392,26 @@ module fdtd_mem_ctrl
                     s_CS_n         = INIT_READ_HY;
                 end
                 else 
-                    s_CS_n = WAIT_WRITE_EZ;
+                    s_CS_n = WAIT_WRITE_EZ;*/
+                if (~s_w_gnt)begin
+                    s_CS_n = WAIT_WRITE_EZ; 
+                end
+                else begin
+					if (r_word_size == 'd0)begin
+                        mem_rd_end_o = 1'b1;
+                        s_CS_n       = WAIT_1;
+                    end
+                    else if (nxt_buffer_en)
+                        begin
+                            wt_Ez_sgl      = 1'b0;
+                            buffer_Hy_start_o = 1'b1;
+                            mem_rd_Ez_en_o = 1'b0;
+                            mem_rd_end_o   = 1'b1;
+                            s_CS_n         = INIT_READ_HY;
+                        end
+                    else 
+                        s_CS_n = WAIT_WRITE_EZ;
+                end
             end
 
             WAIT_1:
@@ -393,14 +431,12 @@ module fdtd_mem_ctrl
     
             WAIT_READ_SRC:
             begin
-                s_r_req           = 1'b1;
-                rd_src_sgl        = 1'b1;
+                rd_src_sgl = 1'b1;
                 if (~s_r_gnt)begin
                     s_CS_n = WAIT_READ_SRC;
                 end
                 else if (s_r_gnt)
                 begin
-                    s_r_req           = 1'b0;
                     buffer_src_start_o= 1'b0;
                     buffer_src_end_o  = 1'b1;
                     s_CS_n            = INIT_CALC_PROCESS;
@@ -416,30 +452,29 @@ module fdtd_mem_ctrl
     
             WAIT_WRITE_SRC:
             begin
-                s_w_req = 1'b1;
                 wt_src_sgl = 1'b1;
                 if (~s_w_gnt)begin
-                    mem_rd_end_o    = 1'b1;
-                    s_w_data_store  = 1'b1;
+                    mem_rd_end_o   = 1'b1;
+                    s_w_data_store = 1'b1;
                     s_CS_n = WAIT_WRITE_SRC;
                 end
                 else if(s_w_gnt)
                 begin
-                    wt_src_sgl      = 1'b0;
-                    mem_rd_Ez_en_o  = 1'b0;
-                    mem_rd_end_o    = 1'b1;
-                    s_CS_n          = WAIT_2;
-                    status_busy_o   = 1'b0;
+                    wt_src_sgl     = 1'b0;
+                    mem_rd_Ez_en_o = 1'b0;
+                    mem_rd_end_o   = 1'b1;
+                    s_CS_n         = WAIT_2;
+                    status_busy_o  = 1'b0;
                 end
             end
                 
             WAIT_2:
             begin
 				if (calc_Hy_start_en_i)begin
-                    s_CS_n        = IDLE;
+                    s_CS_n = IDLE;
                 end
                 else 
-                    s_CS_n        = WAIT_2;
+                    s_CS_n = WAIT_2;
             end
     
             default:
@@ -448,6 +483,44 @@ module fdtd_mem_ctrl
                 end
         endcase
     end
+    //
+    always_ff @(posedge ACLK,negedge ARESETn)begin
+        if (!ARESETn)begin
+            s_r_req <= 1'b0;
+            s_w_req <= 1'b0;
+        end
+        else case(r_CS)
+            INIT_READ_HY : 
+            begin
+                s_r_req <= 1'b1; s_w_req <= 1'b0;
+            end
+            INIT_READ_EZ : 
+            begin
+                s_r_req <= 1'b1; s_w_req <= 1'b0;
+            end
+            INIT_WRITE_HY : 
+            begin
+                s_r_req <= 1'b0; s_w_req <= 1'b1;
+            end
+            INIT_WRITE_EZ : 
+            begin
+                s_r_req <= 1'b0; s_w_req <= 1'b1;
+            end
+            INIT_READ_SRC :
+            begin
+                s_r_req <= 1'b1; s_w_req <= 1'b0;
+            end
+            INIT_WRITE_SRC : 
+            begin
+                s_r_req <= 1'b0; s_w_req <= 1'b1;
+            end
+            default : 
+            begin
+                s_r_req <= 1'b0; s_w_req <= 1'b0;
+            end
+        endcase
+    end
+
     //
     always_ff @(posedge ACLK,negedge ARESETn)begin
         if (!ARESETn)
@@ -463,7 +536,7 @@ module fdtd_mem_ctrl
     always_ff @(posedge ACLK,negedge ARESETn)begin
         if (!ARESETn)
             calc_Ez_end_flg <= 1'b0;
-        else if (r_word_size == 'd1 && r_CS == WAIT_WRITE_EZ)
+        else if (r_word_size == 'd0 && r_CS == WAIT_WRITE_EZ)
             calc_Ez_end_flg <= 1'b1;
         else if (calc_Ez_end_flg == 1'b1 && calc_src_start_en_i )
             calc_Ez_end_flg <= 1'b0;
@@ -517,14 +590,14 @@ module fdtd_mem_ctrl
                 r_w_Ez_word_addr <= 'b0;
                 r_word_size      <= 'b0;
             end
-            else case(r_CS)
+            else case(s_CS_n)
             IDLE:
             begin
-                r_r_Hy_word_addr <= Hy_addr_i[mstr.AXI_ADDR_WIDTH-1:2];
-                r_r_Ez_word_addr <= Ez_addr_i[mstr.AXI_ADDR_WIDTH-1:2];
-                r_r_Ez_word_addr_0 <= Ez_addr_i[mstr.AXI_ADDR_WIDTH-1:2]+1'b1;
-                r_w_Hy_word_addr <= Hy_addr_i[mstr.AXI_ADDR_WIDTH-1:2];
-                r_w_Ez_word_addr <= Ez_addr_i[mstr.AXI_ADDR_WIDTH-1:2]+1'b1;
+                r_r_Hy_word_addr <= Hy_addr_i[mstr.AXI_ADDR_WIDTH-1:0];
+                r_r_Ez_word_addr <= Ez_addr_i[mstr.AXI_ADDR_WIDTH-1:0];
+                r_r_Ez_word_addr_0 <= Ez_addr_i[mstr.AXI_ADDR_WIDTH-1:0]+1'b1;
+                r_w_Hy_word_addr <= Hy_addr_i[mstr.AXI_ADDR_WIDTH-1:0];
+                r_w_Ez_word_addr <= Ez_addr_i[mstr.AXI_ADDR_WIDTH-1:0]+1'b1;
                 r_word_size      <= size_i;
             end
             
@@ -534,46 +607,46 @@ module fdtd_mem_ctrl
                     r_r_Hy_word_addr <= r_r_Hy_word_addr -1'b1;
                 else 
                     r_r_Hy_word_addr <= (mstr.ar_valid && mstr.ar_ready)
-                                        ? (r_r_Hy_word_addr + 1'b1) : r_r_Hy_word_addr; 
+                                        ? (r_r_Hy_word_addr + burst_size_byte) : r_r_Hy_word_addr; 
             end
             
             WAIT_READ_EZ:
             begin
                 if (calc_Ez_start_en_i)begin 
                     r_r_Ez_word_addr_0 <= (mstr.ar_valid && mstr.ar_ready) 
-                                        ? (r_r_Ez_word_addr_0 + 1'b1) : r_r_Ez_word_addr_0;
+                                        ? (r_r_Ez_word_addr_0 + 1'b1 ) : r_r_Ez_word_addr_0;
                 end	
                 if(buffer_Ez_last_0)
                     r_r_Ez_word_addr <= r_r_Ez_word_addr - 1'b1;
                 else 
                     r_r_Ez_word_addr <= (mstr.ar_valid && mstr.ar_ready)
-                                        ? (r_r_Ez_word_addr + 1'b1) : r_r_Ez_word_addr;
+                                        ? (r_r_Ez_word_addr + burst_size_byte) : r_r_Ez_word_addr;
             end
             
             WAIT_WRITE_HY:
             begin
                 r_w_Hy_word_addr <= (mstr.aw_valid && mstr.aw_ready)
-                                        ? (r_w_Hy_word_addr + 1'b1) : r_w_Hy_word_addr; 
+                                        ? (r_w_Hy_word_addr + burst_size_byte) : r_w_Hy_word_addr; 
                 r_word_size      <= (mstr.aw_valid && mstr.aw_ready)
-                                        ? (r_word_size - 1'b1) : r_word_size;
+                                        ? (r_word_size - axi4_lenth) : r_word_size;
             end
             
             WAIT_WRITE_EZ:
             begin
                 r_w_Ez_word_addr <= (mstr.aw_valid && mstr.aw_ready)
-                                        ? (r_w_Ez_word_addr + 1'b1) : r_w_Ez_word_addr; 
+                                        ? (r_w_Ez_word_addr + burst_size_byte) : r_w_Ez_word_addr; 
                 r_word_size      <= (mstr.aw_valid && mstr.aw_ready)
-                                        ? (r_word_size - 1'b1) : r_word_size;
+                                        ? (r_word_size - axi4_lenth) : r_word_size;
             end
             
             INIT_READ_SRC:
             begin
-                r_r_Ez_word_addr <= Ez_addr_i[mstr.AXI_ADDR_WIDTH-1:2];
+                r_r_Ez_word_addr <= Ez_addr_i[mstr.AXI_ADDR_WIDTH-1:0];
             end
             
             INIT_WRITE_SRC:
             begin
-                r_w_Ez_word_addr <= Ez_addr_i[mstr.AXI_ADDR_WIDTH-1:2];
+                r_w_Ez_word_addr <= Ez_addr_i[mstr.AXI_ADDR_WIDTH-1:0];
             end
             
             default:
@@ -588,7 +661,7 @@ module fdtd_mem_ctrl
             endcase
     end
     ///////////////////////
-    fdtd_mem_word_rd
+    fdtd_mem_rd
     #(
         .AXI4_ADDR_WIDTH ( mstr.AXI_ADDR_WIDTH ),
         .AXI4_DATA_WIDTH ( mstr.AXI_DATA_WIDTH ),
@@ -621,14 +694,15 @@ module fdtd_mem_ctrl
         .RUSER_i    ( mstr.r_user    ),
         .RVALID_i   ( mstr.r_valid   ),
         .RREADY_o   ( mstr.r_ready   ),
-
+        
+		.axi_lenth_i    ( axi4_lenth    ),
         .rd_req_i       ( s_r_req       ),
         .rd_word_addr_i ( r_r_word_addr ),
         .rd_data_o      ( s_r_data      ),
         .rd_gnt_o       ( s_r_gnt       )
     );
 
-    fdtd_mem_word_wt
+    fdtd_mem_wt
     #(
         .AXI4_ADDR_WIDTH ( mstr.AXI_ADDR_WIDTH ),
         .AXI4_DATA_WIDTH ( mstr.AXI_DATA_WIDTH ),
@@ -666,7 +740,8 @@ module fdtd_mem_ctrl
         .BVALID_i   ( mstr.b_valid   ),
         .BUSER_i    ( mstr.b_user    ),
         .BREADY_o   ( mstr.b_ready   ),
-
+        
+		.axi_lenth_i    ( axi4_lenth    ),
         .wt_req_i       ( s_w_req       ),
         .wt_word_addr_i ( r_w_word_addr ),
         .wt_data_i      ( r_w_data      ),
